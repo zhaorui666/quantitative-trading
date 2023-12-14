abc430f0971ed9c0886837191b6a1abee5251c5d
zhaorui <1171086487@qq.com>
2023-12-14 21:36:48 +0800

    项目基础信息修改

--- src/main/java/com/zr/mapper/StockBaseInfoMapper.java
+++ src/main/java/com/zr/mapper/StockBaseInfoMapper.java
@@ -1,9 +1,13 @@
 package com.zr.mapper;
 
 import com.zr.pojo.StockBaseInfo;
+import org.apache.ibatis.annotations.Mapper;
+
 import java.util.List;
 
+@Mapper
 public interface StockBaseInfoMapper {
+
     int insert(StockBaseInfo record);
 
     List<StockBaseInfo> selectAll();
